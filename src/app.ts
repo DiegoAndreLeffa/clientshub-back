ac7418cf634e1b735e2592617f0a8719ca613a22
DiegoAndre Leffa <diegodocanto@gmail.com>
2023-05-29 15:44:11 -0300

    feat: create doc

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import "express-async-errors";
 import cors from "cors";
 import express, { Application } from "express";
+import swaggerUi from "swagger-ui-express";
+
+import swaggerDocs from "./swagger.json";
 
 import { handleErrors } from "./errors";
 
@@ -12,6 +15,9 @@ export const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.use("/register", userRouter);
 app.use("/login", loginRouter);
 
